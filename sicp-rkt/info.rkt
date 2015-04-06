007049d73699342e0bd5fe9e767af3d519dceb2a
manhtai <vo@manhtai.com>
2015-04-06 14:19:55 +0700

    add sicp package

---
+++ sicp-rkt/info.rkt
@@ -0,0 +1,15 @@
+#lang info
+(define collection "sicp-rkt")
+(define name               "SICP")
+(define blurb              '("SICP Support for DrRacket"))
+(define homepage           "http://www.neilvandyke.org/racket-sicp/")
+(define categories         '(misc))
+(define can-be-loaded-with 'all)
+
+(define required-core-version "5.0")
+(define repositories          '("4.x"))
+
+(define tools              '("sicp-tool.rkt"))
+(define tool-names         '("SICP"))
+(define tool-urls          '("http://www.neilvandyke.org/racket-sicp/"))
+
