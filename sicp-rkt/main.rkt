789e42080fecc54ae80b5e58021b038947dc5ed2
manhtai <vo@manhtai.com>
2015-04-06 16:18:37 +0700

    update rkt

--- sicp-rkt/main.rkt
+++ sicp-rkt/main.rkt
@@ -5,7 +5,9 @@
                  error
                  flush-output
                  make-parameter
+                 random
                  void?)
+           (rename racket/base racket:module-begin #%module-begin)
            (only (planet williams/science:4:=8/science) random-integer) 
            ;; Import random-integer for generating number larger than 4294967087
            ;; but it require exact integer, so we don't use (random 1e12), 
@@ -13,11 +15,7 @@
            (all-except (planet soegaard/sicp:2:=1/sicp) cons-stream))
            ;; Import sicp picture language for draw picture in racket
 
-;;; @some frequent procedures
-
-(define-syntax sicp:error
-  (syntax-rules ()
-    ((_ REASON ARG ...) (error REASON ARG ...))))
+;;; Arithmetic
 
 (define true #t)
 
@@ -31,12 +29,55 @@
 
 (define (dec x) (- x 1))
 
+(define (average a b) (/ (+ a b) 2))
+
+(define (cube x) (* x x x))
+
 (define (square x) (* x x))
+
+(define (expmod b exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod b (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* b (expmod b (- exp 1) m))
+          m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random-integer (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= 0 times) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(define (prime? n)
+  (fast-prime? n 100))
+
+(define pi/4 (atan 1 1))
+(define pi (* 4 pi/4))
+
+;;; Utilities
 
 (define (runtime)
   (inexact->exact (truncate (* 1000 (current-inexact-milliseconds)))))
 
-;;; @section Streams
+(define (compose f g)
+  (define (f*g x)
+    (f (g x)))
+  f*g)
+
+(define (repeated f n)
+  (cond ((= n 0) identity)
+	((= n 1) f)
+	(else (compose f (repeated f (- n 1))))))
+
+;;; Streams
 
 (define-syntax cons-stream
   (syntax-rules ()
@@ -46,7 +87,11 @@
 
 (define (stream-null? x) (null? x))
 
-;;; @section Syntax
+;;; Syntax
+
+(define-syntax sicp:error
+  (syntax-rules ()
+    ((_ REASON ARG ...) (error REASON ARG ...))))
 
 (define-syntax sicp-syntax-error
   (syntax-rules ()
@@ -90,7 +135,8 @@
 
 (#%provide
  (for-syntax syntax-rules ...)
- (all-from-except r5rs)
+;; (all-from-except r5rs #%module-begin)
+ (rename racket:module-begin #%module-begin)
  (all-from (planet williams/science:4:=8/science))
  (all-from (planet soegaard/sicp:2:=1/sicp))
  (rename sicp:error  error)
@@ -105,7 +151,12 @@
  nil
  runtime
  square
+ cube
+ prime?
+ pi
+ average
+ compose
+ repeated
  stream-null?
  the-empty-stream
  cons-stream)
-
